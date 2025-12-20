6aca9ac53c4d9054deb502294dc9266811607c74
Christian Jensen <christianjensen91@gmail.com>
2025-12-20 20:14:04 +0100

    #245 put back ups history

--- src/main/kotlin/com/krillsson/sysapi/graphql/UpsResolver.kt
+++ src/main/kotlin/com/krillsson/sysapi/graphql/UpsResolver.kt
@@ -1,14 +1,20 @@
 package com.krillsson.sysapi.graphql
 
-import com.krillsson.sysapi.ups.UpsDevice
 import com.krillsson.sysapi.ups.UpsService
+import com.krillsson.sysapi.ups.UpsDevice
+import com.krillsson.sysapi.ups.UpsMetricsHistoryEntry
+import com.krillsson.sysapi.ups.UpsMetricsHistoryRepository
+import java.time.Instant
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.stereotype.Controller
 
 @Controller
 @SchemaMapping(typeName = "UpsInfoAvailable")
-class UpsResolver(val upsService: UpsService) {
+class UpsResolver(
+    val upsService: UpsService,
+    val upsMetricsHistoryRepository: UpsMetricsHistoryRepository
+) {
 
     @SchemaMapping
     fun upsDevices(): List<UpsDevice> {
@@ -18,5 +24,14 @@ class UpsResolver(val upsService: UpsService) {
     @SchemaMapping
     fun upsDeviceById(@Argument id: String): UpsDevice? {
         return upsService.upsDeviceByName(id)
+    }
+
+    @SchemaMapping
+    fun upsMetricsHistoryBetweenTimestamps(
+        @Argument id: String,
+        @Argument from: Instant,
+        @Argument to: Instant
+    ): List<UpsMetricsHistoryEntry> {
+        return upsMetricsHistoryRepository.getHistoryLimitedToDates(id, from, to)
     }
 }
