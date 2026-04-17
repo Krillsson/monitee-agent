da55e6add86e147bede5284d476470fed6987468
Christian Jensen <christianjensen91@gmail.com>
2026-04-17 19:59:13 +0200

    Fix powershell

--- .github/actions/inno-setup-download/entrypoint.ps1
+++ .github/actions/inno-setup-download/entrypoint.ps1
@@ -1,41 +1,12 @@
 Set-Location $Env:TEMP
 
-Write-Host "InnoSetup: Getting lastest innoextract release"
-$extract_release = Invoke-WebRequest https://api.github.com/repos/dscharrer/innoextract/releases/latest | ConvertFrom-Json
-
-$extract_assets = $extract_release.assets
-$extract_url = ""
-
-Write-Host "InnoSetup: Getting windows zip"
-foreach ($asset in $extract_assets) {
-	if ($asset.name -match '-windows\.zip$') {
-		Write-Host "InnoSetup: Windows zip found name ${asset.name}"
-		$extract_url = $asset.browser_download_url
-	}
-}
-
-if (-not $extract_url) {
-	Write-Host "InnoSetup: Windows zip not found"
-	exit 1
-}
-
-Write-Host "InnoSetup: Downloading innoextract"
-Invoke-WebRequest -URI $extract_url -OutFile innoextract.zip
-Write-Host "InnoSetup: Extracting innoextract.zip"
-Expand-Archive innoextract.zip -DestinationPath innoextract
-Set-Alias -Name Inno-Extract -Value "$(Get-Location)\innoextract\innoextract.exe"
-
-Write-Host "InnoSetup: Downloading InnoSetup executable"
+Write-Host "InnoSetup: Downloading InnoSetup $($env:IS_VERSION)"
 $tag_version = ${env:IS_VERSION}.replace('.', '_')
 $dl_url = "https://github.com/jrsoftware/issrc/releases/download/is-${tag_version}/innosetup-${env:IS_VERSION}.exe"
 Invoke-WebRequest -URI $dl_url -OutFile inno.exe
 
-Write-Host "InnoSetup: Extracting InnoSetup"
-Inno-Extract inno.exe --output-dir ./ --include app
-Rename-Item app -NewName inno
-
-Write-Host "InnoSetup: Downloading encryption dll"
-Invoke-WebRequest -URI https://jrsoftware.org/download.php/iscrypt.dll -OutFile .\inno\ISCrypt.dll
+Write-Host "InnoSetup: Installing InnoSetup silently"
+Start-Process -FilePath ".\inno.exe" -ArgumentList "/VERYSILENT", "/SUPPRESSMSGBOXES", "/NORESTART", "/SP-" -Wait
 
 Write-Host "InnoSetup: Adding InnoSetup to path"
-Add-Content $Env:GITHUB_PATH "$(Get-Location)\inno"
+Add-Content $Env:GITHUB_PATH "C:\Program Files (x86)\Inno Setup 6"
