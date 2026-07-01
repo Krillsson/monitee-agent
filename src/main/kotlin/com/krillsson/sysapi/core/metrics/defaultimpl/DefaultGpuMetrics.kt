c148d6e809a9f56388528867d88b8d5b5e813749
Christian Jensen <christianjensen91@gmail.com>
2026-07-01 15:05:32 +0200

    #168: implement DefaultGpuLoadMetrics ticks sampling + DefaultGpuMetrics

--- src/main/kotlin/com/krillsson/sysapi/core/metrics/defaultimpl/DefaultGpuMetrics.kt
+++ src/main/kotlin/com/krillsson/sysapi/core/metrics/defaultimpl/DefaultGpuMetrics.kt
@@ -3,18 +3,37 @@ package com.krillsson.sysapi.core.metrics.defaultimpl
 import com.krillsson.sysapi.core.domain.gpu.Gpu
 import com.krillsson.sysapi.core.domain.gpu.GpuLoad
 import com.krillsson.sysapi.core.metrics.GpuMetrics
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 import oshi.hardware.Display
 import oshi.hardware.HardwareAbstractionLayer
+import reactor.core.publisher.Flux
 
 @Component
-open class DefaultGpuMetrics(private val hal: HardwareAbstractionLayer) : GpuMetrics {
-    override fun gpuLoads(): List<GpuLoad> {
-        return emptyList()
+open class DefaultGpuMetrics(
+    private val hal: HardwareAbstractionLayer,
+    @Qualifier("defaultGpuLoadMetrics") private val gpuLoadMetrics: DefaultGpuLoadMetrics
+) : GpuMetrics {
+    override fun gpus(): List<Gpu> {
+        return hal.graphicsCards.map { card ->
+            Gpu(card.deviceId, card.name, card.vendor, card.versionInfo, card.vRam)
+        }
     }
 
-    override fun gpus(): List<Gpu> {
-        return emptyList()
+    override fun gpuLoads(): List<GpuLoad> {
+        return gpuLoadMetrics.gpuLoads
+    }
+
+    override fun gpuLoadById(id: String): GpuLoad? {
+        return gpuLoadMetrics.gpuLoadById(id)
+    }
+
+    override fun gpuLoadEvents(): Flux<List<GpuLoad>> {
+        return gpuLoadMetrics.gpuLoadEvents()
+    }
+
+    override fun gpuLoadEventsById(id: String): Flux<GpuLoad> {
+        return gpuLoadMetrics.gpuLoadEventsById(id)
     }
 
     override fun displays(): List<Display> {
--- src/main/kotlin/com/krillsson/sysapi/core/metrics/windows/WindowsGpuMetrics.kt
+++ src/main/kotlin/com/krillsson/sysapi/core/metrics/windows/WindowsGpuMetrics.kt
@@ -1,42 +1,13 @@
 package com.krillsson.sysapi.core.metrics.windows
 
-import com.krillsson.sysapi.core.domain.gpu.Gpu
-import com.krillsson.sysapi.core.domain.gpu.GpuHealth
-import com.krillsson.sysapi.core.domain.gpu.GpuLoad
+import com.krillsson.sysapi.core.metrics.defaultimpl.DefaultGpuLoadMetrics
 import com.krillsson.sysapi.core.metrics.defaultimpl.DefaultGpuMetrics
-import com.krillsson.sysapi.core.metrics.windows.util.NullSafeOhmMonitor
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 import oshi.hardware.HardwareAbstractionLayer
-import java.util.stream.Collectors
 
 @Component
-class WindowsGpuMetrics(hal: HardwareAbstractionLayer, private val monitorManager: OHMManager) :
-    DefaultGpuMetrics(hal) {
-    override fun gpus(): List<Gpu> {
-        monitorManager.update()
-        return monitorManager.gpuMonitors().map { monitor ->
-            Gpu(
-                monitor.getVendor(),
-                monitor.getName(),
-                NullSafeOhmMonitor.nullSafeGetValue(monitor.getCoreClock()),
-                NullSafeOhmMonitor.nullSafeGetValue(monitor.getMemoryClock())
-            )
-        }
-    }
-
-    override fun gpuLoads(): List<GpuLoad> {
-        monitorManager.update()
-        return monitorManager.gpuMonitors().map { monitor ->
-            GpuLoad(
-                monitor.getName(), NullSafeOhmMonitor.nullSafeGetValue(monitor.getCoreLoad()),
-                NullSafeOhmMonitor.nullSafeGetValue(monitor.getMemoryLoad()),
-                GpuHealth(
-                    NullSafeOhmMonitor.nullSafeGetValue(monitor.getFanRPM()),
-                    NullSafeOhmMonitor.nullSafeGetValue(monitor.getFanPercent()),
-                    NullSafeOhmMonitor.nullSafeGetValue(monitor.getTemperature())
-                )
-            )
-        }
-
-    }
-}
+class WindowsGpuMetrics(
+    hal: HardwareAbstractionLayer,
+    @Qualifier("defaultGpuLoadMetrics") gpuLoadMetrics: DefaultGpuLoadMetrics
+) : DefaultGpuMetrics(hal, gpuLoadMetrics)
