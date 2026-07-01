aa2ea6ea45cd74a2dbbbcc0a10ecad5b532d1fcd
Christian Jensen <christianjensen91@gmail.com>
2026-07-01 15:05:32 +0200

    #168: add gpu load events to GpuMetrics interface + caching

--- src/main/kotlin/com/krillsson/sysapi/core/metrics/GpuMetrics.kt
+++ src/main/kotlin/com/krillsson/sysapi/core/metrics/GpuMetrics.kt
@@ -3,9 +3,13 @@ package com.krillsson.sysapi.core.metrics
 import com.krillsson.sysapi.core.domain.gpu.Gpu
 import com.krillsson.sysapi.core.domain.gpu.GpuLoad
 import oshi.hardware.Display
+import reactor.core.publisher.Flux
 
 interface GpuMetrics {
     fun gpus(): List<Gpu>
     fun displays(): List<Display>
     fun gpuLoads(): List<GpuLoad>
+    fun gpuLoadById(id: String): GpuLoad?
+    fun gpuLoadEvents(): Flux<List<GpuLoad>>
+    fun gpuLoadEventsById(id: String): Flux<GpuLoad>
 }
--- src/main/kotlin/com/krillsson/sysapi/core/metrics/cache/CachingGpuMetrics.kt
+++ src/main/kotlin/com/krillsson/sysapi/core/metrics/cache/CachingGpuMetrics.kt
@@ -2,14 +2,18 @@ package com.krillsson.sysapi.core.metrics.cache
 
 import com.google.common.base.Supplier
 import com.google.common.base.Suppliers
+import com.google.common.cache.CacheBuilder
+import com.google.common.cache.CacheLoader
+import com.google.common.cache.LoadingCache
 import com.krillsson.sysapi.config.CacheConfiguration
 import com.krillsson.sysapi.core.domain.gpu.Gpu
 import com.krillsson.sysapi.core.domain.gpu.GpuLoad
 import com.krillsson.sysapi.core.metrics.GpuMetrics
 import oshi.hardware.Display
+import reactor.core.publisher.Flux
 
 class CachingGpuMetrics(
-    gpuMetrics: GpuMetrics,
+    private val gpuMetrics: GpuMetrics,
     cacheConfiguration: CacheConfiguration
 ) : GpuMetrics {
     private val gpusCache: Supplier<List<Gpu>> = Suppliers.memoizeWithExpiration(
@@ -24,6 +28,14 @@ class CachingGpuMetrics(
         Suppliers.synchronizedSupplier { gpuMetrics.gpuLoads() },
         cacheConfiguration.duration, cacheConfiguration.unit
     )
+    private val gpuLoadsQueryCache: LoadingCache<String, GpuLoad?> = CacheBuilder.newBuilder()
+        .expireAfterWrite(cacheConfiguration.duration, cacheConfiguration.unit)
+        .build(object : CacheLoader<String, GpuLoad?>() {
+            @Throws(Exception::class)
+            override fun load(key: String): GpuLoad? {
+                return gpuMetrics.gpuLoadById(key)
+            }
+        })
 
     override fun gpus(): List<Gpu> {
         return gpusCache.get()
@@ -35,5 +47,17 @@ class CachingGpuMetrics(
 
     override fun gpuLoads(): List<GpuLoad> {
         return gpuLoadsCache.get()
+    }
+
+    override fun gpuLoadById(id: String): GpuLoad? {
+        return gpuLoadsQueryCache.getUnchecked(id)
+    }
+
+    override fun gpuLoadEvents(): Flux<List<GpuLoad>> {
+        return gpuMetrics.gpuLoadEvents()
+    }
+
+    override fun gpuLoadEventsById(id: String): Flux<GpuLoad> {
+        return gpuMetrics.gpuLoadEventsById(id)
     }
 }
